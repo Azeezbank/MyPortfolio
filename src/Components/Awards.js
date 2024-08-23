2caf0bcb40cd10453a0de9388b9c3acfe353d6fe
Azeezbank <159575592+Azeezbank@users.noreply.github.com>
2024-08-23 17:39:20 +0100

    Update Awards.js

--- src/Components/Awards.js
+++ src/Components/Awards.js
@@ -145,6 +145,15 @@ const Awards = () => {
               <p>Power Learn Project</p>
             </div>
           </div>
+            <div className='row mt-4'>
+              <div className='col-sm-6 pt-2'>
+                <p className='text-primary'>2017 - 2023</p>
+              </div>
+              <div className='col-sm-6'>
+                <h5>Bachelor degree</h5>
+                <p>FUOYE</p>
+              </div>
+            </div>
         </div>
 
         <h2 id="experience" className="fw-bold p-5 pb-1">
